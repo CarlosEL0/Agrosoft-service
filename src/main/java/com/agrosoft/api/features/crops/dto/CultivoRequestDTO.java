19b57da815a8fabe95d5236e6cd0c51908068727
EdwarDa2 <edwardanielgonzalezhernandez@gmail.com>
2026-02-18 18:21:00 -0600

    feat(crop): Creacion entidad,repository y DTO cultivo

---
+++ src/main/java/com/agrosoft/api/features/crops/dto/CultivoRequestDTO.java
@@ -0,0 +1,23 @@
+package com.agrosoft.api.features.crops.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+public class CultivoRequestDTO {
+    private UUID idUsuario;
+    private String nombreCultivo;
+    private String tipoCultivo;
+    private LocalDate fechaSiembra;
+    private String notasGenerales;
+
+    private BigDecimal alturaEsperada;
+    private Integer diasGerminacion;
+    private Integer diasVegetativo;
+    private Integer diasFloracion;
+    private Integer diasCosecha;
+    private BigDecimal phSueloMin;
+    private BigDecimal phSueloMax;
+}

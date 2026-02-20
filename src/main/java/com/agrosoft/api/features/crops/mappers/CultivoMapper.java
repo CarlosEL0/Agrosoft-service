6bddb2a1e1f56f0a7220e6c57f846b9cf78b6c66
EdwarDa2 <edwardanielgonzalezhernandez@gmail.com>
2026-02-19 22:37:47 -0600

    Refactor(crop): Configuraccion y optimizacion  de mapperCultivo

--- src/main/java/com/agrosoft/api/features/crops/mappers/CultivoMapper.java
+++ src/main/java/com/agrosoft/api/features/crops/mappers/CultivoMapper.java
@@ -2,51 +2,13 @@ package com.agrosoft.api.features.crops.mappers;
 
 import com.agrosoft.api.features.crops.dto.CultivoRequestDTO;
 import com.agrosoft.api.features.crops.entities.CultivoEntity;
-import org.springframework.stereotype.Component;
+import org.mapstruct.*;
 
-@Component
-public class CultivoMapper {
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CultivoMapper {
 
-    public CultivoEntity toEntity(CultivoRequestDTO dto) {
-        if (dto == null) {
-            return null;
-        }
+    CultivoEntity toEntity(CultivoRequestDTO dto);
 
-        return CultivoEntity.builder()
-                .idUsuario(dto.getIdUsuario())
-                .nombreCultivo(dto.getNombreCultivo())
-                .tipoCultivo(dto.getTipoCultivo())
-                .fechaSiembra(dto.getFechaSiembra())
-                .notasGenerales(dto.getNotasGenerales())
-                .alturaEsperada(dto.getAlturaEsperada())
-                .diasGerminacion(dto.getDiasGerminacion())
-                .diasVegetativo(dto.getDiasVegetativo())
-                .diasFloracion(dto.getDiasFloracion())
-                .diasCosecha(dto.getDiasCosecha())
-                .tamanoTerreno(dto.getTamanoTerreno())
-                .cantidadSemillas(dto.getCantidadSemillas())
-                .phSueloMin(dto.getPhSueloMin())
-                .phSueloMax(dto.getPhSueloMax())
-                .build();
-    }
-
-    public void updateEntityFromDto(CultivoRequestDTO dto, CultivoEntity entity) {
-        if (dto == null || entity == null) {
-            return;
-        }
-
-        entity.setNombreCultivo(dto.getNombreCultivo());
-        entity.setTipoCultivo(dto.getTipoCultivo());
-        entity.setFechaSiembra(dto.getFechaSiembra());
-        entity.setNotasGenerales(dto.getNotasGenerales());
-        entity.setAlturaEsperada(dto.getAlturaEsperada());
-        entity.setDiasGerminacion(dto.getDiasGerminacion());
-        entity.setDiasVegetativo(dto.getDiasVegetativo());
-        entity.setDiasFloracion(dto.getDiasFloracion());
-        entity.setDiasCosecha(dto.getDiasCosecha());
-        entity.setCantidadSemillas(dto.getTamanoTerreno());
-        entity.setTamanoTerreno(dto.getCantidadSemillas());
-        entity.setPhSueloMin(dto.getPhSueloMin());
-        entity.setPhSueloMax(dto.getPhSueloMax());
-    }
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(CultivoRequestDTO dto, @MappingTarget CultivoEntity entity);
 }

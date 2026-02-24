cb59686ccc2ce5f5492c1dff4dbf464cf33aafe7
EdwarDa2 <edwardanielgonzalezhernandez@gmail.com>
2026-02-23 23:56:43 -0600

    refactor(crop): Se agrego un metodo al repositorio de Etapa Crecimiento

--- src/main/java/com/agrosoft/api/features/crops/repositories/EtapaCrecimientoRepository.java
+++ src/main/java/com/agrosoft/api/features/crops/repositories/EtapaCrecimientoRepository.java
@@ -4,11 +4,12 @@ import com.agrosoft.api.features.crops.entities.EtapaCrecimientoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EtapaCrecimientoRepository extends JpaRepository<EtapaCrecimientoEntity, UUID> {
-
     List<EtapaCrecimientoEntity> findByIdCicloOrderByOrdenEtapaAsc(UUID idCiclo);
+    List<EtapaCrecimientoEntity> findByFechaFin(LocalDate fechaFin);
 }
